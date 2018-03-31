9ab0e86f2dc3b0c8c4c3730f506de521366b7f46
Umang Galaiya <umanghome@gmail.com>
2018-03-31 22:09:59 +0530

    Deletes duplicate tweet posted from Facebook. (#1)

--- index.js
+++ index.js
@@ -12,12 +12,28 @@ twitter.get('statuses/user_timeline', {
   screen_name: constants.screen_name
 }, (err, tweets, response) => {
   if (tweets) {
+    let prevTweet = null;
     tweets.forEach(tweet => {
       if (tweet.text.indexOf('Retweeted') === 0 && tweet.text.indexOf('#nodelete') < 0) {
         twitter.post('/statuses/destroy/:id', {
           id: tweet.id_str
         }, () => {});
       }
+
+      if (prevTweet) {
+        // Get the first ten characters of both.
+        let prev = prevTweet.text.slice(0, 10);
+        let curr = tweet.text.slice(0, 10);
+
+        // Delete previous tweet if current and previous have the same first-10 chars.
+        if (prev.startsWith(curr)) {
+          twitter.post('/statuses/destroy/:id', {
+            id: prevTweet.id_str
+          });
+        }
+      }
+
+      prevTweet = tweet;
     });
   }
 });
